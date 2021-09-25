d33a23ef8269f0713b062cd36bdcf0ebc010899e
elechigeorge <elechigeorgepro.com>
2021-09-25 21:00:05 +0100

    created frontend resources

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome to Exam Lab Proffessional version 4.0</h2>
+
+        <Button>Click Here to Begin</Button>
       </header>
     </div>
   );
