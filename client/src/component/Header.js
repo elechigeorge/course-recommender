a7b9b23462dab0bc3ab9e96fab0cd64464003d72
elechigeorge <elechigeorgepro@gmail.com>
2021-11-29 11:52:29 -0800

    resolved link

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -72,7 +72,7 @@ const Header = () => {
                       )}
 
                       {adminInfo && (
-                        <NavDropdown.Item href="/student/lists">
+                        <NavDropdown.Item href="/student/list">
                           View Student Profile
                         </NavDropdown.Item>
                       )}
